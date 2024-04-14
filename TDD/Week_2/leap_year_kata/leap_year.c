ffebd9044c961947d94c464f4d9d06a8b13b3fcf
Miguel Jimenez Lopez <mjl.klyone@gmail.com>
2024-04-15 01:30:46 +0200

    TDD/leap_year_kata: [Refactor] Simplify cyclomatic complexity reducing the conditional statements.

--- TDD/Week_2/leap_year_kata/leap_year.c
+++ TDD/Week_2/leap_year_kata/leap_year.c
@@ -2,13 +2,5 @@
 
 int is_leap_year(unsigned int year)
 {
-    if (year % 400 == 0) {
-        return TRUE;
-    }
-    else {
-        if (year % 4 == 0 && year % 100 != 0)
-            return TRUE;
-        else
-            return FALSE;
-    }
+    return ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) ? TRUE : FALSE;
 }
