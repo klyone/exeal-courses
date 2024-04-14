1a77d7f9586229ef043f4171ca5b157a78e925db
Miguel Jimenez Lopez <mjl.klyone@gmail.com>
2024-04-15 01:44:09 +0200

    TDD/string_calculator_kata: Add initial failing test case (Compilation error)

--- TDD/Week_3/string_calculator_kata/test_string_calculator.c
+++ TDD/Week_3/string_calculator_kata/test_string_calculator.c
@@ -4,6 +4,7 @@
 #include <setjmp.h>
 #include <cmocka.h>
 
+
 /**
  *
  * This is a dummy test to ensure that the cmocka is properly configured
@@ -12,9 +13,15 @@
 static void test_cmocka_is_properly_configured(void **state) {
 }
 
+static void test_empty_string_should_sum_zero(void **state) {
+    assert_int_equal(add(""), 0);
+}
+
+
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_cmocka_is_properly_configured),
+        cmocka_unit_test(test_empty_string_should_sum_zero),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
