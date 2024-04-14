5b52193d4262b9ce3a6d0a6cb95cb42386ec4cfc
Miguel Jimenez Lopez <mjl.klyone@gmail.com>
2024-04-15 01:15:25 +0200

    TDD/leap_year_kata: Add new failing test case for year 800.

--- TDD/Week_2/leap_year_kata/test_leap_year.c
+++ TDD/Week_2/leap_year_kata/test_leap_year.c
@@ -24,11 +24,17 @@ static void test_100_is_no_leap_year(void **state)
     assert_int_equal(is_leap_year(100), FALSE);
 }
 
+static void test_800_is_leap_year(void **state)
+{
+    assert_int_equal(is_leap_year(800), TRUE);
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_cmocka_is_properly_configured),
         cmocka_unit_test(test_400_is_leap_year),
         cmocka_unit_test(test_100_is_no_leap_year),
+        cmocka_unit_test(test_800_is_leap_year),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
