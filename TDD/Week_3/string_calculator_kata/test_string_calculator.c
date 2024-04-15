6791e65675938006ba425fd144e859dcb348429e
Miguel Jimenez Lopez <mjl.klyone@gmail.com>
2024-04-15 02:11:27 +0200

    TDD/string_calculator_kata: Add new failing test case with two numbers in the string.

--- TDD/Week_3/string_calculator_kata/test_string_calculator.c
+++ TDD/Week_3/string_calculator_kata/test_string_calculator.c
@@ -22,12 +22,16 @@ static void test_one_number_string_should_sum_same_number(void **state) {
     assert_int_equal(add("3"), 3);
 }
 
+static void test_two_numbers_string_should_add_both_number(void **state) {
+    assert_int_equal(add("3,4"), 7);
+}
 
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_cmocka_is_properly_configured),
         cmocka_unit_test(test_empty_string_should_sum_zero),
         cmocka_unit_test(test_one_number_string_should_sum_same_number),
+        cmocka_unit_test(test_two_numbers_string_should_add_both_number),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
