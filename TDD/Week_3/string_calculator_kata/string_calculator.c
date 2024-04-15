474f3ea8341891f884060aaad7cfececcf77d89a
Miguel Jimenez Lopez <mjl.klyone@gmail.com>
2024-04-15 02:22:45 +0200

    TDD/string_calculator_kata: Add new production code for the end of line separator case.

--- TDD/Week_3/string_calculator_kata/string_calculator.c
+++ TDD/Week_3/string_calculator_kata/string_calculator.c
@@ -9,13 +9,13 @@ int add(const char *numbers)
     char *tmp = calloc(strlen(numbers), sizeof(char));
     strncpy(tmp, numbers, strlen(numbers));
 
-    char *n = strtok(tmp, ",");
+    char *n = strtok(tmp, ",\n");
     int sum = 0;
 
     while (n != NULL) {
        int number = strtol(n, NULL, 0);
        sum += number;
-       n = strtok(NULL, ",");
+       n = strtok(NULL, ",\n");
     }
 
     free(tmp);
