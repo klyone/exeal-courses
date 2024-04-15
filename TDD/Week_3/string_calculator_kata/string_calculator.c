8cb531ab08dc6870110608ffee54faf81ef0828d
Miguel Jimenez Lopez <mjl.klyone@gmail.com>
2024-04-15 02:11:54 +0200

    TDD/string_calculator_kata: Add production code to pass the two numbers string case.

--- TDD/Week_3/string_calculator_kata/string_calculator.c
+++ TDD/Week_3/string_calculator_kata/string_calculator.c
@@ -1,6 +1,24 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "string_calculator.h"
 
 int add(const char *numbers)
 {
-    return strtol(numbers, NULL, 0);
+    char *tmp = calloc(strlen(numbers), sizeof(char));
+    strncpy(tmp, numbers, strlen(numbers));
+
+    char *n = strtok(tmp, ",");
+    int sum = 0;
+
+    while (n != NULL) {
+       int number = strtol(n, NULL, 0);
+       sum += number;
+       n = strtok(NULL, ",");
+    }
+
+    free(tmp);
+
+    return sum;
 }
