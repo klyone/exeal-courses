601840433c889c796abc1327376ff08e2fb76e63
Miguel Jimenez Lopez <mjl.klyone@gmail.com>
2024-04-20 03:04:29 +0200

    TDD/string_calculator_kata(iteration1): [RED] Add new test for a variant sequence of numbers in the string.

--- TDD/Week_3/string_calculator_kata/iteration1/test_string_calculator.c
+++ TDD/Week_3/string_calculator_kata/iteration1/test_string_calculator.c
@@ -22,9 +22,13 @@ static void add_a_string_with_one_number_return_this_number(void **state) {
     assert_int_equal(Add("3"), 3);
 }
 
-// TODO: Implement the basic Add method with two numbers
 static void add_a_string_with_two_number_return_the_sum(void **state) {
     assert_int_equal(Add("3,5"), 8);
+}
+
+// TODO: Handle more than two numbers in the string
+static void add_a_string_with_several_number_return_the_sum(void **state) {
+    assert_int_equal(Add("3,5,7"), 15);
 }
 
 int main(void) {
@@ -33,6 +37,7 @@ int main(void) {
         cmocka_unit_test(add_with_empty_string_is_zero),
         cmocka_unit_test(add_a_string_with_one_number_return_this_number),
         cmocka_unit_test(add_a_string_with_two_number_return_the_sum),
+        cmocka_unit_test(add_a_string_with_several_number_return_the_sum),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
