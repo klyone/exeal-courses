aebbf90abb2e282b5b58eefa212637158194f385
Miguel Jimenez Lopez <mjl.klyone@gmail.com>
2024-04-20 02:45:14 +0200

    TDD/string_calculator_kata(iteration1): [RED] Add a new test case for a string with two integers.

--- TDD/Week_3/string_calculator_kata/iteration1/test_string_calculator.c
+++ TDD/Week_3/string_calculator_kata/iteration1/test_string_calculator.c
@@ -18,18 +18,21 @@ static void add_with_empty_string_is_zero(void **state) {
     assert_int_equal(Add(""), 0);
 }
 
-// TODO: Implement the basic Add method with one number
 static void add_a_string_with_one_number_return_this_number(void **state) {
     assert_int_equal(Add("3"), 3);
 }
 
 // TODO: Implement the basic Add method with two numbers
+static void add_a_string_with_two_number_return_the_sum(void **state) {
+    assert_int_equal(Add("3,5"), 8);
+}
 
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_cmocka_is_properly_configured),
         cmocka_unit_test(add_with_empty_string_is_zero),
         cmocka_unit_test(add_a_string_with_one_number_return_this_number),
+        cmocka_unit_test(add_a_string_with_two_number_return_the_sum),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
