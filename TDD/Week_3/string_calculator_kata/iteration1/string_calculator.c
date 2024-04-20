231f4c6e6c194c2321d25d0bca2b9426b0d23add
Miguel Jimenez Lopez <mjl.klyone@gmail.com>
2024-04-20 03:18:32 +0200

    TDD/string_calculator_kata(iteration1): [REFACTOR] Extract a new function to detect if a char is a separator.

--- TDD/Week_3/string_calculator_kata/iteration1/string_calculator.c
+++ TDD/Week_3/string_calculator_kata/iteration1/string_calculator.c
@@ -3,6 +3,11 @@
 
 #include "string_calculator.h"
 
+static int is_separator(char c)
+{
+    return c == ',' || c == '\n';
+}
+
 int Add(const char *string)
 {
     int number = 0;
@@ -11,7 +16,7 @@ int Add(const char *string)
     int i;
 
     for(i = 0 ; i < len ; i++) {
-        if(string[i] != ',' && string[i] != '\n' && sscanf(&string[i], "%d", &number))
+        if(!is_separator(string[i]) && sscanf(&string[i], "%d", &number))
             sum += number;
     }
 
