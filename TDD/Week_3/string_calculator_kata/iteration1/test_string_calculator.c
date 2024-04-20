b1f1a165ef729b2b5ac459fe1dacba2d29d12e16
Miguel Jimenez Lopez <mjl.klyone@gmail.com>
2024-04-20 02:32:35 +0200

    TDD/string_calculator_kata(iteration1): [RED] Add a test for a string with a single number.

--- TDD/Week_3/string_calculator_kata/iteration1/test_string_calculator.c
+++ TDD/Week_3/string_calculator_kata/iteration1/test_string_calculator.c
@@ -19,13 +19,17 @@ static void add_with_empty_string_is_zero(void **state) {
 }
 
 // TODO: Implement the basic Add method with one number
+static void add_a_string_with_one_number_return_this_number(void **state) {
+    assert_int_equal(Add("3"), 3);
+}
+
 // TODO: Implement the basic Add method with two numbers
 
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_cmocka_is_properly_configured),
         cmocka_unit_test(add_with_empty_string_is_zero),
-
+        cmocka_unit_test(add_a_string_with_one_number_return_this_number),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
